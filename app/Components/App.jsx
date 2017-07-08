97497c4e89f4c47b1a877e4087052ec4435adf7e
Pragya Singh <pragyasingh991@gmail.com>
2017-07-08 10:38:39 +1000

    first commit

---
+++ app/Components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Menu from './menu/index.jsx'
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div><Menu/></div>)
+  }
+}
---
+++ app/Components/menu/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+//import  './menu.scss';
+export default class Menu extends React.Component {
+    render(){
+        return ( <div>singh</div>)
+    }
+}
