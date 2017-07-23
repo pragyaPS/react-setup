8f5511d35a337bcd8967f75d742fb8ad4d37b061
Pragya Singh <pragyasingh991@gmail.com>
2017-07-24 07:11:57 +1000

    added redux

--- app/Components/App.jsx
+++ app/Components/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createStore, applyMiddleware } from 'redux';
+import { provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import Menu from './menu/index.jsx'
+import { ajaxLoadData } from '../shared/utils/utils.js';
 
 export default class App extends React.Component {
+  static propTypes = {
+    dataUrl: React.PropTypes.string
+  };
+   componentDidMount() {
+        console.log(this.props.dataUrl);
+        ajaxLoadData(this.props.dataUrl, (data) => {
+            console.log("inside comp", data);
+        });
+    }
+
   render() {
     return (
-     <div><Menu/></div>)
+     <div><Menu /></div>);
   }
 }
---
+++ app/shared/constants.js
@@ -0,0 +1,3 @@
+export const Employee_HasError = "Employee_HasError"
+export const Employee_Is_Loading = "Employee_Is_Loading";
+export const Employee_Fetch_Data_Success = "Employee_Fetch_Data_Success";
